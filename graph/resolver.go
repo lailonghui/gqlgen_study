2cabecb46cf29e7bbec242dc390722fa4cc82dc3
lailonghui <624758224@qq.com>
2020-11-04 17:14:01 +0800

    subscription complete Real-time chat system can be based on subscription

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,8 +1,36 @@
 //go:generate go run github.com/99designs/gqlgen
 package graph
 
+import (
+	"context"
+	"lai.com/gqlgen_study/graph/model"
+	"math/rand"
+	"sync"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type ckey string
+type Resolver struct {
+	Rooms map[string]*model.Chatroom
+	mu    sync.Mutex // nolint: structcheck
+}
+
+func getUsername(ctx context.Context) string {
+	if username, ok := ctx.Value(ckey("username")).(string); ok {
+		return username
+	}
+	return ""
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
--- server.go
+++ server.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/websocket"
+	"lai.com/gqlgen_study/graph"
+	"lai.com/gqlgen_study/graph/generated"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"lai.com/gqlgen_study/graph"
-	"lai.com/gqlgen_study/graph/generated"
+	"time"
 )
 
 const defaultPort = "8080"
@@ -20,10 +22,19 @@ func main() {
 	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	})
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+
 }
