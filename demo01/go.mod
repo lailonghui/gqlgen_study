7db6b15fb5be9e69b2152956d20c8077ed040dc6
lailonghui <624758224@qq.com>
2020-11-05 16:31:11 +0800

    base completed

--- demo01/go.mod
+++ demo01/go.mod
@@ -1,9 +1,10 @@
-module lai.com/gqlgen_study
+module lai.com/gqlgen_study/demo01
 
 go 1.13
 
 require (
 	github.com/99designs/gqlgen v0.13.0
+	github.com/gorilla/websocket v1.4.2
 	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/stretchr/testify v1.5.1 // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0
--- demo01/graph/resolver.go
+++ demo01/graph/resolver.go
@@ -1,36 +1,12 @@
 //go:generate go run github.com/99designs/gqlgen
 package graph
 
-import (
-	"context"
-	"lai.com/gqlgen_study/graph/model"
-	"math/rand"
-	"sync"
-)
+import "lai.com/gqlgen_study/demo01/graph/model"
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type ckey string
 type Resolver struct {
-	Rooms map[string]*model.Chatroom
-	mu    sync.Mutex // nolint: structcheck
-}
-
-func getUsername(ctx context.Context) string {
-	if username, ok := ctx.Value(ckey("username")).(string); ok {
-		return username
-	}
-	return ""
-}
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func randString(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-	return string(b)
+	PhotoList []*model.Photo
 }
