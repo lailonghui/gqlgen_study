6b0752de38b0663a93a3f774e53ab2c327132476
lailonghui <624758224@qq.com>
2020-11-05 16:34:13 +0800

    create demo02

---
+++ demo02/go.mod
@@ -0,0 +1,12 @@
+module lai.com/gqlgen_study/demo01
+
+go 1.13
+
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/gorilla/websocket v1.4.2
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/vektah/gqlparser/v2 v2.1.0
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ demo02/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/websocket"
+	"lai.com/gqlgen_study/demo01/graph"
+	"lai.com/gqlgen_study/demo01/graph/generated"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	})
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
