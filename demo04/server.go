e15721729a359df8c8f4d3539a4dc5dd2a46c640
lailonghui <624758224@qq.com>
2020-11-11 17:33:35 +0800

    demo4 create

---
+++ demo04/server.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"lai.com/gqlgen_study/demo04/graph/generated"
+	"lai.com/gqlgen_study/demo04/graph/resolvers"
+	"net/http"
+	"time"
+)
+
+var mb int64 = 1 << 20
+
+func main() {
+	// Setting up Gin
+	r := gin.Default()
+	r.POST("/query", graphqlHandler())
+	r.GET("/", playgroundHandler())
+	r.Run(":8080")
+}
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	//NewExecutableSchema and Config are in the generated.go file
+	//Resolver is in the resolver.go file
+	//使用NewDefaultServer自动开启自省功能
+	config := generated.Config{Resolvers: &resolvers.Resolver{}}
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(config))
+	h.AddTransport(transport.POST{})
+
+	//开启websocket功能
+	h.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	})
+	//设置上传文件的大小
+	h.AddTransport(transport.MultipartForm{
+		//此选项指定用于将请求正文解析为内存中的multipart/form-data的最大字节数，其余部分存储在磁盘上的临时文件中
+		MaxMemory: 32 * mb,
+		//此选项指定用于将请求正文解析为multipart/form-data的最大字节数。
+		MaxUploadSize: 50 * mb,
+	})
+
+	//开启服务器的自省
+	h.AddTransport(transport.Options{})
+	h.Use(extension.Introspection{})
+
+	//禁用身份验证的自省
+	//h.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
+	//	graphql.GetOperationContext(ctx).DisableIntrospection = true
+	//	return next(ctx)
+	//})
+
+	//设置查询复杂度
+	//h.Use(extension.FixedComplexityLimit(5))
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
